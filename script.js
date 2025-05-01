3ba5b4c50094b76f5d0361fbc7fcb0c53c699be4
Bridget Sibanda <130451071+Bridgettaki@users.noreply.github.com>
2025-05-01 20:31:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const signupBtn = document.getElementById("signupBtn");
+
+    signupBtn.addEventListener("click", function () {
+      alert("Thank you for signing up for the Monthly Subscription!");
+    });
+  });
